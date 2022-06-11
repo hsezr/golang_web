233f036ca773052d088346feb753911b592c125f
Hsezr <hsezrhu@gmail.com>
2022-06-11 16:38:46 +0800

    added

--- AppInit/serviceHelper.go
+++ AppInit/serviceHelper.go
@@ -15,8 +15,8 @@ type EncodeRequestFunc func(*gin.Context) (interface{}, error)
 //怎么处理业务结果
 type DecodeResponseFunc func(*gin.Context, interface{}) error
 
-func RegisterHandler(endpoint Endpoint, ) func(ctx *gin.Context) {
-
+func RegisterHandler(endpoint Endpoint, encodeFunc EncodeRequestFunc, decodeFunc DecodeResponseFunc) func(ctx *gin.Context) {
+	
 	return func(ctx *gin.Context) {
 
 	}
